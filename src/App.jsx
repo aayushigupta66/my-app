6d460c6a2c4f57278b55d4d9007ae74d310f3ca6
Aayushi Gupta <aayushigupta66@gmail.com>
2025-03-03 00:52:26 -0800

    website under construction

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import linkedinLogo from './assets/linkedin.png'
 import './App.css'
 
 function App() {
@@ -8,26 +9,13 @@ function App() {
 
   return (
     <>
+     <h1>Website coming soon.</h1>
+      <h2>In the meantime, check out my LinkedIn — thanks for your patience!</h2>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://www.linkedin.com/in/aayushigupta66/" target="_blank">
+          <img src={linkedinLogo} className="logo" alt="Linkedin logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
